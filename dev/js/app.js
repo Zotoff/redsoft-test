1ba97a222f3ec1473ca558cd174f8a293f8a25b9
Zotoff <paulzotoff@gmail.com>
2020-01-29 15:51:49 +0300

    Starting axios

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,1 +1,31 @@
-alert('check');
+const axios = require('axios').default;
+let buttonBuy = document.querySelectorAll('.picture__price__right a');
+
+document.addEventListener('DOMContentLoaded', function() {
+  buttonBuy.forEach((item) => {
+    item.addEventListener('click', function(e) {
+      e.preventDefault();
+      item.innerHTML = '<img src=\'img/spinner.gif\' alt=\'\'/>';
+      axiosQuery('https://reqres.in/api/products/3');
+    });
+  });
+
+  function axiosQuery(url) {
+    axios.get(url)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  }
+
+});
+
+
+
